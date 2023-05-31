fa66f4728871755e93a78c053590f4a15ab3fc40
charan9515 <bareddycharankumarreddy@gmail.com>
2023-05-31 14:45:54 +0530

    commit

---
+++ java.js
@@ -0,0 +1,14 @@
+// dom methods
+// .getElementById()
+// .betelementbyclassname()
+// .getelementsbytagName()
+// function pawan1(){
+//     let x=parseInt(document.getElementById("pa1").value)
+    
+//     let y=parseInt(document.getElementById("id1").value)
+//     document.getElementById("id2").value=x+y
+// }
+// function print(){
+//     var name="pawan"
+
+// }
